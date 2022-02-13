1f2dc1864a79e2ad91bd7db0457fb9713bf98bb3
two-dimensional-array <max.mazyr2012@gmail.com>
2022-02-13 12:19:58 +0200

    Commentaries are added in header file of class Logger.

--- logger.h
+++ logger.h
@@ -7,17 +7,21 @@
 
 using std::string;
 
+//This class is implement by pattern Singlton.
+//For getting pointer on class call static method GetLogger().
+//Example : Logger* logger = Logger::GetLogger();
+
 class Logger : public ILogger
 {
   public:
-    static Logger* GetLogger(void);
+    static Logger* GetLogger(void);                                        // Return pointer on Logger.
     static Logger* GetLogger(const STDOutConfig& config);
     static Logger* GetLogger(const STDOutMode& mode, const string& path);
     static Logger* GetLogger(const STDOutMode& mode, const char* path);
     void SetSTDOutConfig(const STDOutConfig& config);
     void SetSTDOutConfig(const STDOutMode& mode, const string& path);
     void SetSTDOutConfig(const STDOutMode& mode, const char* path);
-    virtual void PrintMessage(const string& message) override;
+    virtual void PrintMessage(const string& message) override;             // Print message in log file.
     virtual void PrintMessage(const char* message) override;
   private:
     Logger(void);
