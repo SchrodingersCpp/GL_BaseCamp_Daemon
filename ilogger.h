27f81cf41161eaa575f9ba9960cef1df9109b7a2
two-dimensional-array <max.mazyr2012@gmail.com>
2022-02-16 14:01:59 +0200

    Using namespaces are deleted from file ilogger.h .

--- ilogger.h
+++ ilogger.h
@@ -1,7 +1,5 @@
 #include <string>
 
-using std::string;
-
 //This class is implement pattern Singlton.
 //For getting pointer on class call static method GetLogger()
 //Example : Logger* logger = Logger::GetLogger();
@@ -9,5 +7,5 @@ using std::string;
 class ILogger
 {
   public:
-    virtual void PrintMessage(const string& message) = 0;  //Print message in log
+    virtual void PrintMessage(const std::string& message) = 0;  //Print message in log
 };
