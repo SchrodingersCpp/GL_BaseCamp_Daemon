5bb14b7fe34e2e4220d11bb962518a319ee7d5d8
Schrödinger's Cat <18612775+SchrodingersCpp@users.noreply.github.com>
2022-02-21 17:43:53 +0200

    Parser class update

--- parser.h
+++ parser.h
@@ -5,19 +5,8 @@
 
 #include <string>
 #include <vector>
-
-enum class yamlFlags
-{
-  kFlagProcess = 0,
-  kFlagName,
-  kFlagExePath,
-  kFlagStdoutConfig,
-  kFlagFile,
-  kFlagMode,
-  kFlagCmdArgs,
-  kFlagOptionName,
-  kTotal,
-};
+#include <map>
+#include <utility>
 
 class Parser: public IParser
 {
@@ -25,20 +14,44 @@ class Parser: public IParser
   Parser();
   
   void SetFilePath(const std::string& path);
-  std::vector<DataProcess>* GetProcessData();
+  std::vector<DataProcess> GetProcessData();
   
  private:
+  enum class yamlFlags_
+  {
+    kFlagProcess = 0,
+    kFlagName,
+    kFlagExePath,
+    kFlagStdoutConfig,
+    kFlagFile,
+    kFlagMode,
+    kFlagCmdArgs,
+    kFlagOptionName,
+    kTotal,
+  };
+  std::map<yamlFlags_, std::string> valid_yaml_flags_;
   std::string yaml_file_path_;
   std::vector<std::string> yaml_content_;
-  std::string valid_flags_[static_cast<int>(yamlFlags::kTotal)];
+  std::vector<std::pair<std::string, std::string>> yaml_flag_value_;
   std::vector<DataProcess> procs_;
   
+  void AddValidFlag(const yamlFlags_& kFlag, const std::string& flagName);
   void ReadYAMLFile();
+  bool EmptyOrWhitespaceLine(const std::string& line);
   void YAMLValidityCheck();
-  void FillProcesses();
-  void GetFlagValue(std::string& flag, std::string& line,
-                    std::string& flagValue);
-  void FillProcessStruct(std::string& flag, std::string& flagValue);
+  void SingleColonCheck(const std::string& line);
+  void AddFlagNameValuePair(const std::string& line);
+  void ProcessFlagCheck();
+  std::string GetFlagName(const std::string& line);
+  std::string GetFlagValue(const std::string& line);
+  int GetFlagNameBegin(const std::string& line);
+  int GetFlagNameEnd(const std::string& line);
+  void ValidFlagsCheck();
+  void GetFlagNameIndices(std::vector<int>& flagNamePos);
+  void SingleFlagPerProcCheck(std::vector<int>& flagNamePos);
+  bool FlagOccurences(const std::vector<std::string>& flagSlice);
+  void ModeValueCheck();
+  void FillProcs();
 };
 
 #endif // PARSER_H
